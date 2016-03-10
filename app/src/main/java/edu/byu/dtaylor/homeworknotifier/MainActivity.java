f9dd3758ff9e70eb6d4588a4f98a7522bc051979
tanmad21 <tanner@modernizedmedia.com>
2016-03-09 21:32:51 -0700

    basic login activity and beginning of main activity

--- app/src/main/java/edu/byu/dtaylor/homeworknotifier/MainActivity.java
+++ app/src/main/java/edu/byu/dtaylor/homeworknotifier/MainActivity.java
@@ -5,48 +5,35 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.byu.dtaylor.homeworknotifier.helpers.*;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    //This will be replaced by what we pull from the server - ie real data
+    private String assignments = "[{\"Name\":\"Assignment One\",\"Date\":\"2016-03-05\"}," +
+            "{\"Name\":\"Assignment Two\",\"Date\":\"2016-03-06\"}]";
+
+    /*
+    {
+        date:...,
+        assignments:[name1,name2,name2]
+    }
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Schedule schedule = ScheduleFactory.create(assignments);
+        //TODO
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ app/src/main/java/edu/byu/dtaylor/homeworknotifier/helpers/Schedule.java
@@ -0,0 +1,21 @@
+package edu.byu.dtaylor.homeworknotifier.helpers;
+
+/**
+ * Created by Tanner on 3/9/2016.
+ */
+public class Schedule {
+
+
+
+    public Schedule() {
+
+    }
+
+    public void add() {
+        //TODO
+    }
+
+    public void markDone() {
+        //TODO
+    }
+}
