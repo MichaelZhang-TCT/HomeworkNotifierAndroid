4a0f10959ffa7b84175f58b53d8074caed7f9153
David Taylor <longlostbro@gmail.com>
2016-04-03 21:30:36 -0600

    added update database methods.

--- app/src/main/java/edu/byu/dtaylor/homeworknotifier/database/DatabaseHelper.java
+++ app/src/main/java/edu/byu/dtaylor/homeworknotifier/database/DatabaseHelper.java
@@ -5,12 +5,14 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import edu.byu.dtaylor.homeworknotifier.MainActivity;
+import edu.byu.dtaylor.homeworknotifier.Utils;
 import edu.byu.dtaylor.homeworknotifier.gsontools.GsonAssignment;
 import edu.byu.dtaylor.homeworknotifier.gsontools.GsonCourse;
 import edu.byu.dtaylor.homeworknotifier.gsontools.GsonDatabase;
@@ -110,6 +112,76 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             Log.e("DatabaseHelper","Failed to load database");
             return null;
         }
+    }
+
+    public void updateDatabaseAsync(final Context context)
+    {
+        new AsyncTask<Void,Void,GsonDatabase>()
+        {
+            @Override
+            protected GsonDatabase doInBackground(Void... params) {
+                GsonDatabase gsonDb = Utils.getAllInfo(context, "daviddt2", "davidpaseo3");
+                return gsonDb;
+            }
+
+            @Override
+            protected void onPostExecute(GsonDatabase db) {
+                DatabaseHelper dbHelper = new DatabaseHelper(context);
+                dbHelper.updateDatabase(db);
+            }
+        }.execute();
+    }
+
+    private boolean updateDatabase(GsonDatabase gsonDb) {
+        ArrayList<Task> tasks = MainActivity.database.getTasks();
+        SQLiteDatabase db = this.getWritableDatabase();
+        onUpgrade(db, -1, -1);
+        GsonUser user = gsonDb.getUser();
+        List<GsonCourse> courses = user.getCourses();
+
+        for(GsonCourse c : courses) {
+            ContentValues courseValues = new ContentValues();
+            courseValues.put(HomeworkNotifierContract.Classes.COLUMN_NAME_EXTERNAL_COURSE_ID,c.getId());
+            courseValues.put(HomeworkNotifierContract.Classes.COLUMN_NAME_TITLE,c.getTitle());
+            courseValues.put(HomeworkNotifierContract.Classes.COLUMN_NAME_SHORT_TITLE, c.getShortTitle());
+
+            long classId = db.insert(HomeworkNotifierContract.Classes.TABLE_NAME,null,courseValues);
+            List<GsonAssignment> assignments = c.getAssignments();
+            for(GsonAssignment a : assignments) {
+                ContentValues assignmentValues = new ContentValues();
+                Log.d("Tanner", a.getName());
+                if(a.getId() == "?") {
+                    continue;
+                }
+
+                assignmentValues.put(HomeworkNotifierContract.Assignments.COLUMN_NAME_CLASS_ID,classId);
+                assignmentValues.put(HomeworkNotifierContract.Assignments.COLUMN_NAME_EXTERNAL_ASSIGNMENT_ID,a.getId());
+                assignmentValues.put(HomeworkNotifierContract.Assignments.COLUMN_NAME_EXTERNAL_COURSE_ID,a.getCourseID());
+                assignmentValues.put(HomeworkNotifierContract.Assignments.COLUMN_NAME_NAME,a.getName());
+                assignmentValues.put(HomeworkNotifierContract.Assignments.COLUMN_NAME_DESCRIPTION,a.getDescription());
+                assignmentValues.put(HomeworkNotifierContract.Assignments.COLUMN_NAME_CATEGORY,a.getCategory());
+                assignmentValues.put(HomeworkNotifierContract.Assignments.COLUMN_NAME_CATEGORY_ID,a.getCategoryID());
+                assignmentValues.put(HomeworkNotifierContract.Assignments.COLUMN_NAME_DUE_DATE,a.getDueDate());
+                assignmentValues.put(HomeworkNotifierContract.Assignments.COLUMN_NAME_GRADED,a.getGraded());
+                assignmentValues.put(HomeworkNotifierContract.Assignments.COLUMN_NAME_POINTS,a.getPoints());
+                assignmentValues.put(HomeworkNotifierContract.Assignments.COLUMN_NAME_WEIGHT,a.getWeight());
+                assignmentValues.put(HomeworkNotifierContract.Assignments.COLUMN_NAME_TYPE,a.getType());
+                assignmentValues.put(HomeworkNotifierContract.Assignments.COLUMN_NAME_REF_URL,a.getUrl());
+
+                db.insert(HomeworkNotifierContract.Assignments.TABLE_NAME,null,assignmentValues);
+            }
+        }
+        for(Task task : tasks)
+        {
+            insertTask(task);
+        }
+        Database data = getDatabaseFromSql();
+        if(MainActivity.database != null)
+        {
+            MainActivity.database = data;
+            return true;
+        }
+        return false;
     }
 
     public ArrayList<Course> getCourses() {
