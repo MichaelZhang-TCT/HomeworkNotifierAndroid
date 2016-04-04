d5a5aef67372766b6cbd5f71f8a86dd23bf0d9f0
gopher8 <nicholas.victoryofthepeople@gmail.com>
2016-04-04 13:42:02 -0600

    added createNotification function, doesn't work yet

--- app/src/main/java/edu/byu/dtaylor/homeworknotifier/MainActivity.java
+++ app/src/main/java/edu/byu/dtaylor/homeworknotifier/MainActivity.java
@@ -1,6 +1,10 @@
 package edu.byu.dtaylor.homeworknotifier;
 
 import android.app.Fragment;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Rect;
@@ -229,6 +233,44 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 //        fragmentTransaction.add(R.id.scheduleFragmentContainer,new ScheduleFragment());
 //        fragmentTransaction.commit();
 
+    }
+
+    private void setNotification(long showTime){
+
+//                long showAt = System.currentTimeMillis();//immediately
+//                long showAt = System.currentTimeMillis()+30000;
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+//                Notification notification = new Notification(icon,title,showAt);
+        // use System.currentTimeMillis() to have a unique ID for the pending intent
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), (int) System.currentTimeMillis(), new Intent(), 0);
+        Notification notification = new Notification();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            notification = new Notification.Builder(MainActivity.this)
+                    .setTicker("Ticker text")
+                    .setContentTitle("Homework Notification")
+                    .setContentText("You have a homework assignment due at ...")
+                    .setSmallIcon(R.drawable.ic_food_apple_white_18dp)
+                    .setWhen(System.currentTimeMillis())
+                    .setContentIntent(pendingIntent).build();
+        }
+
+//                notification.defaults |= Notification.DEFAULT_SOUND;
+        //use the above default or set custom valuse as below
+//                notification.sound = Uri.parse("file:///sdcard/notification/robo_da.mp3");
+//                notification.defaults |= Notification.DEFAULT_VIBRATE;
+        //use the above default or set custom valuse as below
+//                long[] vibrate = {0,200,100,200};
+//                notification.vibrate = vibrate;
+//                notification.defaults |= Notification.DEFAULT_LIGHTS;
+        //use the above default or set custom valuse as below
+//                notification.ledARGB = 0xffff0000;//red color
+//                notification.ledOnMS = 400;
+//                notification.ledOffMS = 500;
+//                notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+
+        final int notificationIdentifier = 0; //a unique number set by developer to identify a notification, using this notification can be updated/replaced
+        notificationManager.notify(notificationIdentifier, notification);
     }
 
     private void initializeCalendar() {
